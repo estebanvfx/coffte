edd33b0079b08a2231779a41ce26ac9db28a2b36
EstebanVfx <goglemelamama@gmail.com>
2024-09-15 20:51:55 -0500

    Navbar add

--- src/components/ReactComponents/MissionStatement.jsx
+++ src/components/ReactComponents/MissionStatement.jsx
@@ -7,7 +7,7 @@ import { FaLeaf } from "react-icons/fa";
 const MissionStatement = () => {
 
     return (
-        <section className="font-[Poppins] bg-gradient-to-b from-[#fefede] to-green-100">
+        <section className="font-[Poppins] bg-gradient-to-b from-[#fefede] to-yellow-100">
             <span className='px-44 flex items-center justify-center gap-3'><hr className='w-[50%] border-green-900' /> <FaLeaf className='text-green-950 text-5xl' />  <hr className='w-[50%] border-green-900' /></span>
             <div className="px-80 py-20">
                 <h2 className="text-5xl font-bold text-[#1e1e20] mb-6 uppercase">Nuestra Misión</h2>
